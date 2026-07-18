b4819d4d031f234c480c6ce063d4ac212ef4ffb6
Blessy Christrus D <blessychristrus21@gmail.com>
2026-07-18 13:44:00 +0530

    Time: 3 ms (86.94%) | Memory: 43 MB (93.58%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,24 +1,22 @@
-import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
-
-        Stack<Character> stack = new Stack<>();
-
-        for (char ch : s.toCharArray()) {
-
-            if (ch == '(')
-                stack.push(')');
-            else if (ch == '{')
-                stack.push('}');
-            else if (ch == '[')
-                stack.push(']');
-            else {
-                if (stack.isEmpty() || stack.pop() != ch)
+        Stack<Character> sa = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                sa.push(')');
+            }
+            else if(ch == '['){
+                sa.push(']');
+            }
+            else if(ch == '{'){
+                sa.push('}');
+            }
+            else{
+                if(sa.isEmpty() || sa.pop() != ch){
                     return false;
+                }
             }
         }
-
-        return stack.isEmpty();
+        return sa.isEmpty();
     }
 }
