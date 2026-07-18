b1978afefb9ed2bd569e5a641649f6980c424301
Blessy Christrus D <blessychristrus21@gmail.com>
2026-07-18 13:57:33 +0530

    Time: 122 ms (11.99%) | Memory: 48.9 MB (10.29%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(stack.isEmpty()){
+                stack.push(ch);
+            }
+            else if(stack.peek() == ch){
+               stack.pop();
+            }
+            else{
+                stack.push(ch);
+            }
+        }
+        String res = "";
+        for(char c : stack){
+            res+=c;
+        }
+        return res;
+    }
+}
