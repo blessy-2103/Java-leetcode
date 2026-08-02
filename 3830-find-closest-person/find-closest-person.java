ffbf4759e98d9e54cb25683583df85f8ee5cf6d0
Blessy Christrus D <blessychristrus21@gmail.com>
2026-08-02 10:40:56 +0530

    Time: 1 ms (93.77%) | Memory: 42.6 MB (43.22%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int a = Math.abs(z - x);
+        int b = Math.abs(z - y);
+        
+        if(a>b){
+            return 2;
+        }
+        else if(a<b){
+            return 1;
+        }
+    return 0;
+    }
+}
