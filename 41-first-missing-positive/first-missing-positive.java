33cb4516ed8c87b49339dc83d6b34bb89f775550
Blessy Christrus D <blessychristrus21@gmail.com>
2026-08-02 12:50:35 +0530

    Time: 14 ms (25.77%) | Memory: 93.4 MB (6.12%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        
+        for (int num : nums) {
+            set.add(num);
+        }
+        
+       
+        int missing = 1;
+        while (set.contains(missing)) {
+            missing++;
+        }
+        
+        return missing;
+    }
+}
