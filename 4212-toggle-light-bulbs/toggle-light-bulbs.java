9a945c90e5642b53ccb7c67053ad6803398d399d
Blessy Christrus D <blessychristrus21@gmail.com>
2026-09-18 14:44:32 +0530

    Time: 7 ms (45.75%) | Memory: 47.1 MB (10.27%) - LeetSync

---
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> toggleLightBulbs(List<Integer> bulbs) {
+        ArrayList<Integer> list = new ArrayList<>();
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(int i : bulbs){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue()%2!=0){
+                list.add(entry.getKey());
+            }
+        }
+        return list;
+    }
+}
