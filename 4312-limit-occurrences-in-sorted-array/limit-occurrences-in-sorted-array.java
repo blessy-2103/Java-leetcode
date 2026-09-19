0e9e85085c984bd5c5621986a66c15f720f9af97
Blessy Christrus D <blessychristrus21@gmail.com>
2026-09-19 11:25:40 +0530

    Time: 6 ms (12.86%) | Memory: 47.2 MB (8.94%) - LeetSync

--- 4312-limit-occurrences-in-sorted-array/limit-occurrences-in-sorted-array.java
+++ 4312-limit-occurrences-in-sorted-array/limit-occurrences-in-sorted-array.java
@@ -1,24 +1,30 @@
 class Solution {
     public int[] limitOccurrences(int[] nums, int k) {
-        ArrayList<Integer> list = new ArrayList<>();
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i : nums){
             map.put(i,map.getOrDefault(i,0)+1);
         }
+        ArrayList<Integer> list = new ArrayList<>();
         for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            if(entry.getValue()>k){
+            if(entry.getValue() == k){
                 for(int i = 0;i<k;i++){
                     list.add(entry.getKey());
                 }
             }
-            else if(entry.getValue()<= k){
-              for(int i = 0;i<entry.getValue();i++){
-                list.add(entry.getKey());
-              }
-            }
+                else if(entry.getValue() > k){
+                    for(int i = 0;i<k;i++){
+                        list.add(entry.getKey());
+                    }
+                }
+                else if(entry.getValue()<k){
+                    for(int i = 0;i<entry.getValue();i++){
+                        list.add(entry.getKey());
+                    }
+                }
+            
         }
         Collections.sort(list);
-        int[] arr= new int[list.size()];
+        int[] arr = new int[list.size()];
         for(int i = 0;i<list.size();i++){
             arr[i] = list.get(i);
         }
