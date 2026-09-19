8aaa2115908eeb62d3c740a13d3dbc82dc3838dc
Blessy Christrus D <blessychristrus21@gmail.com>
2026-09-19 11:14:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (67.63%) - LeetSync

--- 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.java
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.java
@@ -1,8 +1,8 @@
 class Solution {
     public int firstMatchingIndex(String s) {
         int left = 0;
-        int right = s.length()-1;
-        while(left<=right){
+        int right = s.length() -1;
+        while(left <= right){
             if(s.charAt(left) == s.charAt(right)){
                 return left;
             }
